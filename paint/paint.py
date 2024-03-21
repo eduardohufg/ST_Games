0b14ac22b4eee0a02bc2b4a21e68b9d53a9e5f27
HayabusaLA <luis604@outlook.com>
2024-03-21 09:53:07 -0600

    Rectangulo y Traiangulo

--- paint/paint.py
+++ paint/paint.py
@@ -42,13 +42,35 @@ def circle(start, end):
 
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
+    """Dibuja un rectángulo desde el punto de inicio hasta el punto final."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
+
 
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
+    """Dibuja un triángulo desde el punto de inicio hasta el punto final."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
+
 
 
 def tap(x, y):
