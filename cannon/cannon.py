69c3c382c252958357487e33ba067dfc9091a783
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2024-03-22 00:08:46 -0600

    cambio de velocidades proyectil

--- cannon/cannon.py
+++ cannon/cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 10
-        speed.y = (y + 200) / 10
+        speed.x = (x + 200) / 10.5
+        speed.y = (y + 200) / 10.5
 
 
 def inside(xy):
